449f6d9f967eacd0c590ce1926f7ef3c4c00abe6
lilongji <woshililongji@163.com>
2018-04-28 18:02:14 +0800

    init v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # router.js
-a front-end router and DOM render 一个前端路由和DOM渲染
+a simple and naive front-end router and DOM render 一个图样、图乃义务的前端路由和DOM渲染
+
+## Basic Usage
+
+Create a root DOM for route which id is root:
+```html
+<div id="root"></div>
+```
+
+Create a router:
+``` javascript
+const router = new Router();
+```
+
+Create a controller for path:
+``` javascript
+class R1 {
+  constructor() {
+    console.log('init1');
+    this.template = '<p rtClick="this.showAlert1()">1111111111</p>';
+  }
+  $onInit() {
+    console.log('is $onInit');
+  }
+  $beforeMount() {
+    console.log('is $beforeMount');
+  }
+  $afterMount() {
+    console.log('is $afterMount');
+  }
+  $onDestory() {
+    console.log('is $onDestory');
+  }
+  showAlert1() {
+    alert('草拟吗1')
+  }
+}
+```
+
+Create an array for routes, and init a router:
+```javascript
+const routes = [
+  {
+    path: 'R1',
+    controller: R1,
+  },
+  {
+    path: 'R2',
+    controller: R2,
+  },
+];
+router.init(routes);
+```
+
+Life cycle is:
+```javascript
+constructor
+$onInit
+$beforeMount
+$afterMount
+$onDestory
+```
+
+## To do
+双向绑定 数据监听 动态渲染DOM
---
+++ src/Route.js
@@ -0,0 +1,74 @@
+class Router {
+  constructor() {
+    this.routes = {};
+    this.currentUrl = '';
+    this.rootDom = null;
+    window.addEventListener('load', this.refresh.bind(this), false);
+    window.addEventListener('hashchange', this.refresh.bind(this), false);
+  }
+
+  route(path, controller) {
+    this.routes[path] = controller || function () {};
+  }
+
+  refresh() {
+    this.currentUrl = location.hash.slice(1) || '/';
+    if (this.routes[this.currentUrl]) {
+      if (window.routerController) {
+        if (window.routerController.$onDestory) window.routerController.$onDestory();
+        delete window.routerController;
+      }
+      const controller = new this.routes[this.currentUrl]();
+      if (controller.$onInit) controller.$onInit();
+      this.refreshDom(controller);
+    }
+  }
+
+  init(arr) {
+    if (arr && arr instanceof Array) {
+      arr.forEach(route => {
+        this.route(route.path, route.controller);
+      });
+      const rootDom = document.querySelector('#root');
+      this.rootDom = rootDom || null;
+    } else {
+      console.error('no routes exit');
+    }
+  }
+
+  refreshDom(controller) {
+    const template = controller.template;
+    if (template && typeof template === 'string' && this.rootDom) {
+      if (controller.$beforeMount) controller.$beforeMount();
+      this.replaceDom(controller);
+      if (controller.$afterMount) controller.$afterMount();
+    } else {
+      console.error('refreshDom failed: template or rootDom is not exit');
+    }
+  }
+
+  replaceDom(controller) {
+    const template = controller.template;
+    if (this.rootDom.hasChildNodes()) {
+      let childs = this.rootDom.childNodes;
+      for (let i = childs.length - 1; i >= 0; i--) {
+        this.rootDom.removeChild(childs.item(i));
+      }
+    }
+    window.routerController = controller;
+    let templateDom = this.parseDom(template);
+    let fragment = document.createDocumentFragment();
+    fragment.appendChild(templateDom);
+    this.rootDom.appendChild(fragment);
+  }
+
+  parseDom(template) {
+    const elementCreated = document.createElement('div');
+    let newTemplate = null;
+    if (window.routerController) {
+      newTemplate = template.replace(/( )(rt)([A-Za-z]+="|[A-Za-z]+="')(this)/g, (...args) => `${args[1]}on${args[3]}window.routerController`);
+    }
+    elementCreated.innerHTML = newTemplate;
+    return elementCreated;
+  }
+}

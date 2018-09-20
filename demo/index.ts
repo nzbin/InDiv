afd7e307c559abf12b9e3b81d5332e4f1f31418a
lilongji <woshililongji@163.com>
2018-09-20 16:48:26 +0800

    new version: v1.1.0
    1. support nodejs ssr
    2. rewrite virtual DOM
    3. update render template

--- demo/index.ts
+++ demo/index.ts
@@ -438,6 +438,7 @@ class TestComponent implements OnInit {
       <div nv-repeat="let man in state.testArray" nv-key="man.name">
           <div nv-on:click="@show(state.testArray2)">姓名：{{man.name}}</div>
           <div>性别：{{man.sex}}</div>
+          <test-component nv-key="man.name" man="{man.name}"></test-component>
           <input nv-on:click="@show(b, $index)" nv-repeat="let b in state.testArray2" nv-on:input="@showInput($event, $index)" nv-text="b" nv-class="b" />
           <div class="fuck" nv-repeat="let c in man.job" nv-key="c.id">
             <input nv-on:click="@show(c, $index)" nv-model="c.name" nv-class="c.id" />

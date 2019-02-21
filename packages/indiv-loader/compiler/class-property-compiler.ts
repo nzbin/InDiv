0a4876083d6b5b17bff82a210186ee899aab50bb
lilongji <woshililongji@163.com>
2019-02-21 17:16:22 +0800

    fix class-property-compiler

--- packages/indiv-loader/compiler/class-property-compiler.ts
+++ packages/indiv-loader/compiler/class-property-compiler.ts
@@ -1,4 +1,4 @@
-import recast from 'recast';
+import types from 'recast/lib/types';
 
 /**
  * add template in class
@@ -8,12 +8,12 @@ import recast from 'recast';
  * @param {boolean} useTypeScript
  */
 export function classPropertyCompiler(templateInfo: { templateString: string; classBody: any }, useTypeScript: boolean): void {
-  const typeAnnotation = useTypeScript ? recast.types.builders.typeAnnotation(
-    recast.types.builders.genericTypeAnnotation(recast.types.builders.identifier('string'), null),
+  const typeAnnotation = useTypeScript ? types.builders.typeAnnotation(
+    types.builders.genericTypeAnnotation(types.builders.identifier('string'), null),
   ) : null;
-  templateInfo.classBody.body.body.push(recast.types.builders.classProperty(
-    recast.types.builders.identifier('template'),
-    recast.types.builders.identifier(`\`${templateInfo.templateString}\``),
+  templateInfo.classBody.body.body.push(types.builders.classProperty(
+    types.builders.identifier('template'),
+    types.builders.identifier(`\`${templateInfo.templateString}\``),
     typeAnnotation,
   ));
 }
